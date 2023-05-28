5e76f38865dfcace5ea4e8fffb82afd6345337fc
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2023-05-28 03:15:04 -0700

    chrome-extension

---
+++ packages/chrome-extension/rollup.config.mjs
@@ -0,0 +1,34 @@
+import path from "path"
+import {chromeExtension, simpleReloader} from "rollup-plugin-chrome-extension";
+import typescript from "rollup-plugin-typescript2";
+import resolve from '@rollup/plugin-node-resolve'
+import {emptyDir} from "rollup-plugin-empty-dir";
+import commonjs from "@rollup/plugin-commonjs";
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import scss from "rollup-plugin-scss";
+export default {
+    input: 'src/manifest.json',
+    output: {
+        dir: 'dist',
+        format: 'esm',
+        chunkFileNames: path.join('chunks','[name]-[hash].js'),
+    },
+    plugins: [
+        chromeExtension(),
+        simpleReloader(),
+        resolve(),
+        commonjs(),
+        typescript({
+            tsconfig: '../../tsconfig.json',
+            tsconfigOverride: {
+                compilerOptions: {
+                    types:["@types/chrome"]
+                }
+            }
+        }),
+        emptyDir(),
+        scss({ fileName: 'assets/bundle.css'}),
+        sourcemaps()
+    ]
+
+}
---
+++ packages/chrome-extension/src/content/extensions.ts
@@ -0,0 +1,6 @@
+import {ITanaExtension} from "tana-extensions-core/src/TanaExtensionInitializer/types";
+import CodeBlockExtension from "extension-code-block/src";
+
+export default [
+    CodeBlockExtension
+] as ITanaExtension[]
