a6d3ce3e24bcef41e69ffb50bec07fb60c3c8962
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2023-05-28 13:29:35 -0700

    Resolved race condition caused by properties being set before the check occurs

--- packages/core/src/TanaLoader/index.ts
+++ packages/core/src/TanaLoader/index.ts
@@ -3,6 +3,7 @@ import {TANA_MAIN_UI_CONTAINER_CLASS_PREFIX, TANA_REACT_APP_ID} from "./types";
 export default new class TanaLoader {
     public async waitForFieldToInstantiate(object:Object,key:string) {
         return new Promise((resolve) => {
+            if (key in object) return resolve(true)
             Object.defineProperty(object, key, {
                 configurable: true,
                 set(v){
@@ -16,9 +17,10 @@ export default new class TanaLoader {
     public async waitForPageDOMToCompleteInitialization() {
         return new Promise((resolve) => {
             const app = document.getElementById(TANA_REACT_APP_ID)!
+            if (this.mainUIContainerHasBeenAdded(Array.from(app.children) as HTMLElement[])) return resolve(true)
             new MutationObserver((mutationList:MutationRecord[],observer:MutationObserver) => {
                 for (const mutation of mutationList) {
-                    if (this.mainUIContainerHasBeenAdded(mutation)) {
+                    if (this.mainUIContainerHasBeenAdded(Array.from(mutation.addedNodes) as HTMLElement[])) {
                         observer.disconnect()
                         resolve(true)
                     }
@@ -30,15 +32,15 @@ export default new class TanaLoader {
         })
     }
 
-    private mainUIContainerHasBeenAdded(mutation:MutationRecord): boolean {
-        for (const addedNode of Array.from(mutation.addedNodes) as HTMLElement[]){
+
+    private mainUIContainerHasBeenAdded(htmlElements:HTMLElement[]) {
+        for (const htmlElement of htmlElements){
             const mainUIContainer =
-                Array.from(addedNode.classList)
+                Array.from(htmlElement.classList)
                     .find(cssClass => cssClass.match(TANA_MAIN_UI_CONTAINER_CLASS_PREFIX))
             if (!!mainUIContainer) return true
         }
         return false
     }
-
 
 }
