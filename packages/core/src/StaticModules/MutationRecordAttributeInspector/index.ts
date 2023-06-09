3fa652fcb8ea1d9de24e4f6b2532493300b7f56f
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2023-06-09 00:14:35 -0700

    added explicit return types

--- packages/core/src/StaticModules/MutationRecordAttributeInspector/index.ts
+++ packages/core/src/StaticModules/MutationRecordAttributeInspector/index.ts
@@ -15,7 +15,7 @@ export default new class MutationRecordAttributeInspector extends TanaConstants 
         this.getNonTemplateCssClass()
     ]
 
-    public  getMutationEventType(mutation:MutationRecord) {
+    public  getMutationEventType(mutation:MutationRecord): NodeEventTypeEnum {
         const removedNodes = Array.from(mutation.removedNodes) as HTMLElement[]
         const addedNodes = Array.from(mutation.addedNodes) as HTMLElement[]
         const isRemoveEvent = !!TanaDomNodeProvider.getContentNodeFromArray(removedNodes)
@@ -31,7 +31,7 @@ export default new class MutationRecordAttributeInspector extends TanaConstants 
         return NodeEventTypeEnum.Update
     }
 
-    public  mutationInvolvesPanelContainer(mutation:MutationRecord) {
+    public  mutationInvolvesPanelContainer(mutation:MutationRecord):boolean {
         const isRemoval = mutation.removedNodes.length > 0
         const htmlElement = (isRemoval ? mutation.removedNodes[0] : mutation.addedNodes[0]) as HTMLElement
         return Maybe.fromFalsy(htmlElement.childNodes.length > 0 && htmlElement.tagName == "DIV")
@@ -42,26 +42,27 @@ export default new class MutationRecordAttributeInspector extends TanaConstants 
             .orDefault(false)
     }
 
-    public  nodeHasClassPrefix(node:HTMLElement,classPrefix:string) {
+    public  nodeHasClassPrefix(node:HTMLElement,classPrefix:string):boolean {
         if (!node.hasOwnProperty('classList')) return false
         for (const classItem of Array.from(node.classList)) {
             if (!!classItem.match(classPrefix)) return true
         }
         return false
     }
-    public mutationRecordContainsBulletModule(mutation:MutationRecord) {
+    
+    public mutationRecordContainsBulletModule(mutation:MutationRecord):boolean{
         const isRemoval = mutation.removedNodes.length > 0
         const node = (isRemoval ? mutation.removedNodes[0] : mutation.addedNodes[0] ) as HTMLElement
         const bulletModuleSelector = this.getBulletModulePrefix()
         return this.nodeHasClassPrefix(node,bulletModuleSelector)
     }
 
-    public  mutationRecordTargetHasExpectedTagName(target:HTMLElement) {
+    public  mutationRecordTargetHasExpectedTagName(target:HTMLElement):boolean {
         if (!target) return false
         return (target as HTMLElement).tagName == "DIV" || (target as HTMLElement).tagName == "SPAN"
     }
 
-    public  mutationRecordTargetHasExpectedClass({target}:MutationRecord) {
+    public  mutationRecordTargetHasExpectedClass({target}:MutationRecord):boolean {
         for (const classItem of this.classList) {
             if ((target as HTMLElement).classList.contains(classItem)) return true
         }
