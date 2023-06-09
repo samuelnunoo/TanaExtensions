fc96ec74011ceed3063df5cebee463afcd5a28b5
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2023-06-09 00:21:13 -0700

    removed export of internal constants in TanaLoader

--- packages/core/src/StaticModules/TanaLoader/index.ts
+++ packages/core/src/StaticModules/TanaLoader/index.ts
@@ -1,5 +1,5 @@
-export const TANA_REACT_APP_ID = "app"
-export const TANA_MAIN_UI_CONTAINER_CLASS_PREFIX = "Application-module_mainUIContainer"
+const TANA_REACT_APP_ID = "app"
+const TANA_MAIN_UI_CONTAINER_CLASS_PREFIX = "Application-module_mainUIContainer"
 
 export default new class TanaLoader {
     public async waitForFieldToInstantiate(object:Object,key:string) {
