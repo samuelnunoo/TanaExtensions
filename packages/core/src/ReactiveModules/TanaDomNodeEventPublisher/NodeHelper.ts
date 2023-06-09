9ce0743f33f716bfd80c35b2336e9411ec57a13a
Samuel Nunoo <38366722+samuelnunoo@users.noreply.github.com>
2023-06-09 00:28:00 -0700

    refactored the NodeHelper to use TanaConstants instead of rogue Constants

--- packages/core/src/ReactiveModules/TanaDomNodeEventPublisher/NodeHelper.ts
+++ packages/core/src/ReactiveModules/TanaDomNodeEventPublisher/NodeHelper.ts
@@ -1,51 +1,50 @@
-import {
-    BULLET_CONTENT_CSS_CLASS, BULLET_CONTENT_CSS_SELECTOR,
-    CONTENT_SIDE_CSS_CLASS,
-    EDITABLE_CSS_CLASS, EXPANDED_NODE_CSS_CLASS, NodeEventTypeEnum,
-    NodeTargetTypeEnum, NON_TEMPLATE_CSS_CLASS,
-    PANEL_CONTENT_CSS_CLASS
-} from "./types/types";
-import {TANA_DATA_PANEL_CSS_SELECTOR} from "../TanaDomPanelEventPublisher/types/constants";
 import {PanelEvenTypeEnum} from "../TanaDomPanelEventPublisher/types/types";
-import {TANA_WRAPPER_CSS_SELECTOR} from "../../StaticModules/TanaDomNodeProvider/types";
+import TanaConstants from "../../StaticModules/TanaDomNodeProvider/types";
+import { NodeTargetTypeEnum, NodeEventTypeEnum } from "./types/types";
 
+export default new class NodeHelper extends TanaConstants {
 
-export default class NodeHelper {
-    public static getTargetType(target:HTMLElement) {
+    public  getTargetType(target:HTMLElement) {
         if (!('classList' in target)) return null
-        if (target.classList.contains(CONTENT_SIDE_CSS_CLASS)) return NodeTargetTypeEnum.ContentSide
-        if (target.classList.contains(BULLET_CONTENT_CSS_CLASS)) return NodeTargetTypeEnum.BulletAndContent
-        if (target.classList.contains(EDITABLE_CSS_CLASS)) return NodeTargetTypeEnum.Editable
-        if (target.classList.contains(PANEL_CONTENT_CSS_CLASS)) return NodeTargetTypeEnum.PanelContent
-        if (target.classList.contains(NON_TEMPLATE_CSS_CLASS)) return NodeTargetTypeEnum.NonTemplateContent
-        if (target.classList.contains(EXPANDED_NODE_CSS_CLASS)) return NodeTargetTypeEnum.ExpandedNodeContent
+        if (target.classList.contains(this.getContentSideCssClass())) return NodeTargetTypeEnum.ContentSide
+        if (target.classList.contains(this.getContentNodeCssClass())) return NodeTargetTypeEnum.BulletAndContent
+        if (target.classList.contains(this.getEditableNodeCssClass())) return NodeTargetTypeEnum.Editable
+        if (target.classList.contains(this.getPanelContentCssClass())) return NodeTargetTypeEnum.PanelContent
+        if (target.classList.contains(this.getNonTemplateCssClass())) return NodeTargetTypeEnum.NonTemplateContent
+        if (target.classList.contains(this.getExpandedNodeCssClass())) return NodeTargetTypeEnum.ExpandedNodeContent
         return null
     }
 
-    public static getNodeEventType(panelEventType:PanelEvenTypeEnum) {
+    public  getNodeEventType(panelEventType:PanelEvenTypeEnum) {
         return panelEventType == PanelEvenTypeEnum.Deletion
-            ? NodeEventTypeEnum.Deletion :
-            panelEventType == PanelEvenTypeEnum.Insertion ? NodeEventTypeEnum.Insertion
-                : NodeEventTypeEnum.Update
+            ? NodeEventTypeEnum.Deletion 
+            : panelEventType == PanelEvenTypeEnum.Insertion 
+            ? NodeEventTypeEnum.Insertion
+            : NodeEventTypeEnum.Update
     }
 
-    public static getWrapAndEditableNodeElementFromParent(element:HTMLElement) {
-        return element.querySelector(TANA_WRAPPER_CSS_SELECTOR)
+    public  getWrapAndEditableNodeElementFromParent(element:HTMLElement) {
+        const wrapSelector = this.classSelector(this.getTanaWrapperCssClass())
+        return element.querySelector(wrapSelector)
     }
-    public static getBulletAndContentNodeElementFromDescendant(target:HTMLElement, element:HTMLElement, targetType:NodeTargetTypeEnum) {
+
+    public  getBulletAndContentNodeElementFromDescendant(target:HTMLElement, element:HTMLElement, targetType:NodeTargetTypeEnum) {
+        const contentNodeSelector = this.classSelector(this.getContentNodeCssClass())
+        const panelSelector = this.attributeSelector(this.getPanelAttribute())
+
         switch(targetType) {
             case NodeTargetTypeEnum.Editable:
-                return target.closest(BULLET_CONTENT_CSS_SELECTOR) as HTMLElement
+                return target.closest(contentNodeSelector) as HTMLElement
             case NodeTargetTypeEnum.BulletAndContent:
                 return target as HTMLElement
             case NodeTargetTypeEnum.ExpandedNodeContent:
-                return target.closest(BULLET_CONTENT_CSS_SELECTOR) as HTMLElement
+                return target.closest(contentNodeSelector) as HTMLElement
             case NodeTargetTypeEnum.PanelContent:
-                return target.closest(TANA_DATA_PANEL_CSS_SELECTOR) as HTMLElement
+                return target.closest(panelSelector) as HTMLElement
             case NodeTargetTypeEnum.NonTemplateContent:
                 return element as HTMLElement
             case NodeTargetTypeEnum.ContentSide:
-                return element.closest(BULLET_CONTENT_CSS_SELECTOR) as HTMLElement
+                return element.closest(contentNodeSelector) as HTMLElement
             default:
                 return null
         }
